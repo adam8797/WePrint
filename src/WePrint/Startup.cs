5fa1819cb818fdb5ec1b648ae48727f77989f775
Adam Schiavone <adam8797@me.com>
2020-01-13 16:36:58 -0500

    Updated stuff for DB

--- src/WePrint/Startup.cs
+++ src/WePrint/Startup.cs
@@ -67,7 +67,7 @@ namespace WePrint
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Testing"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
@@ -75,7 +75,6 @@ namespace WePrint
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
