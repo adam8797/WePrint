1b245f75b4f325c01749dfb488e32dd6f4e4fc19
Josh Rouzer <jvr29@drexel.edu>
2020-02-28 15:54:46 -0500

    adjusting eslint run methods, add lint-staged

--- src/WePrint/ClientApp/src/components/table/table.jsx
+++ src/WePrint/ClientApp/src/components/table/table.jsx
@@ -1,22 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { useTable } from 'react-table';
+import PropTypes from 'prop-types';
+import { useTable, Column } from 'react-table';
 import './table.scss';
 
-function Table({title, columns, data}) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
+function Table({ title, columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
     columns,
-    data
+    data,
   });
   return (
-    <div class='table'>
-      <span class='table__title'>{title}</span>
-      <table {...getTableProps()} class='table__content'>
+    <div className="table">
+      <span className="table__title">{title}</span>
+      <table {...getTableProps()} className="table__content">
         <thead>
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
@@ -27,20 +23,26 @@ function Table({title, columns, data}) {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row)
+          {rows.map(row => {
+            prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
                 {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
                 })}
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
     </div>
   );
 }
+
+Table.propTypes = {
+  title: PropTypes.string.isRequired,
+  columns: PropTypes.arrayOf(PropTypes.objectOf(Column)).isRequired,
+  data: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
 
 export default Table;
