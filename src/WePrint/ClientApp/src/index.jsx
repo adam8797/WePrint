83b1a0ed8b6b6fcb1862b92a7ca7b1e43b395384
Josh Rouzer <jvr29@drexel.edu>
2020-02-29 21:51:47 -0500

    added font-awesome icons

--- src/WePrint/ClientApp/src/index.jsx
+++ src/WePrint/ClientApp/src/index.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router';
+import './fa-library';
 import './assets/styles/_base.scss';
 // import registerServiceWorker from './registerServiceWorker';
 
--- src/WePrint/ClientApp/src/views/home/home.jsx
+++ src/WePrint/ClientApp/src/views/home/home.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { BodyCard } from '../../components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { BodyCard, Button, SectionTitle } from '../../components';
 import Lorem from './components/lorem';
 
 const HomePage = () => (
   <BodyCard>
+    <SectionTitle title="Home Page" />
     <p>This is the home page</p>
     <p>
       Check out this cool Topic:
       <Link to="/topics/components">Components</Link>
     </p>
+    <Button type={Button.Type.PRIMARY}>Do the thing!</Button>
+    <FontAwesomeIcon icon="coffee" />
+    <FontAwesomeIcon icon={['fab', 'google']} size="lg" />
+    <FontAwesomeIcon icon={['fab', 'microsoft']} spin size="lg" />
+    <FontAwesomeIcon icon="check-square" bordered size="xs" />
     <Lorem />
     <Lorem />
     <Lorem />
     <Lorem />
     <Lorem />
+    <SectionTitle title="Lower Section" />
     <Lorem />
     <Lorem />
   </BodyCard>
