5d76cc589809a2a1d8e61f6809fcc1dcb0df3c49
Adam Schiavone <adam8797@me.com>
2020-03-08 23:06:50 -0400

    Added volume to slice reportsx

--- src/WePrint.Common/Slicer/Models/SliceReport.cs
+++ src/WePrint.Common/Slicer/Models/SliceReport.cs
@@ -5,7 +5,11 @@ namespace WePrint.Common.Slicer.Models
     public class SliceReport
     {
         public string FileName { get; set; }
+
+        public Volume Volume { get; set; }
+
         public ICollection<TimeEstimate> TimeEstimates { get; set; }
+
         public ICollection<MaterialEstimate> MaterialEstimates { get; set; }
     }
 }
