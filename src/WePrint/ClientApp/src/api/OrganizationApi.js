09a9cc8fb4d5e5bc3dd070d8dffda5210e7b436a
Nathan <nph29@drexel.edu>
2020-04-13 17:36:30 -0400

    Lowercase, like a filthy casual who's afraid of the shift key. I mean come on, functions should be uppercase, it just looks better, you know? Anyway, it's lowercase now, but I'm mad about it, especially because tomorrow I know I'm gonna write more code uppercase again, and I'm going to have to dig through it all again. Eh, c'est la vie,  I suppose, that's javascript for you. No solid type system to catch typos, if only that were possible. Nope. Can't be done. Because javascript is a hodge-podge language build on top of another, worse, hodge-podge language. It just gets worse the deeper you go. Anyway, I digress, it's lowercase now. (But also, why did the linter not yell at me for this?)

--- src/WePrint/ClientApp/src/api/OrganizationApi.js
+++ src/WePrint/ClientApp/src/api/OrganizationApi.js
@@ -9,9 +9,9 @@ class OrgApi extends CommonApi {
         super("organizations", OrganizationModel.AllPropsEqual);
 
         // Used to get/add/remove users to an organization
-        this.Users = new NestedApi("organizations", "users", "users", UserModel.AllPropsEqual);
+        this.users = new NestedApi("organizations", "users", "users", UserModel.AllPropsEqual);
         // Used to get/add/remove projects to an organization
-        this.Projects = new NestedApi("organizations", "projects", "projects", ProjectModel.AllPropsEqual);
+        this.projects = new NestedApi("organizations", "projects", "projects", ProjectModel.AllPropsEqual);
     }
 }
 
