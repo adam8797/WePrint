819dececd650c9f450e86ffc9b618e5134235568
Adam Schiavone <adam8797@me.com>
2020-04-14 20:39:11 +0000

    Merged PR 54: WEPRINT-16 Added Application Insights to prod
    Pretty basic. Created the App Insights service in Azure, and linked it to Production only.

--- src/WePrint/Startup.cs
+++ src/WePrint/Startup.cs
@@ -117,6 +117,8 @@ namespace WePrint
                 new Assembly[0]);
 
             services.RegisterPermissions();
+
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
