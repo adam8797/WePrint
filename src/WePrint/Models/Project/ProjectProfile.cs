1c617a0488bbb40c7d848b9646af67b75dab5bfa
James Gilkeson <jhg57@drexel.edu>
2020-04-17 00:32:13 -0400

    Add calculated progress value to Project view model

---
+++ src/WePrint/Models/Project/ProjectProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+using AutoMapper.EquivalencyExpression;
+
+namespace WePrint.Models
+{
+    public class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<Project, ProjectViewModel>()
+                .ForMember(dest => dest.Progress, opt => opt.MapFrom((s,d) => s.Pledges.Select(p => p.Quantity).Sum() / s.Goal * 100));
+            CreateMap<ProjectViewModel, Project>();
+            CreateMap<ProjectCreateModel, Project>();
+            CreateMap<Guid, Project>().ConvertUsing<EntityConverter<Guid, Project>>();
+            CreateMap<Project, Guid>().ConvertUsing(x => x.Id);
+        }
+    }
+}
