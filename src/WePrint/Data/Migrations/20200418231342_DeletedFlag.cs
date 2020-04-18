bbe66df94fabe64a96150e7ee5eebbb9a8744b62
Adam Schiavone <adam8797@me.com>
2020-04-18 19:14:26 -0400

    Deleted flag migration

---
+++ src/WePrint/Data/Migrations/20200418231342_DeletedFlag.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WePrint.Data.Migrations
+{
+    public partial class DeletedFlag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "ProjectUpdates",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Projects",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Printers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Pledges",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Organizations",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "ProjectUpdates");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Printers");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Pledges");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Organizations");
+        }
+    }
+}
