b8eddb0ac04ed8a012e1e5ca2057c0c5c2665fb5
Adam Schiavone <adam8797@me.com>
2020-04-20 14:52:35 -0400

    Added authorization

--- src/WePrint/Areas/Admin/Controllers/OrganizationsController.cs
+++ src/WePrint/Areas/Admin/Controllers/OrganizationsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WePrint.Areas.Admin.Models;
@@ -12,6 +13,7 @@ using WePrint.Models;
 namespace WePrint.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
     public class OrganizationsController : ManagementController<Organization>
     {
         public OrganizationsController(IServiceProvider services) : base(services)
