dec522a9c2a8544456fe9773cd18e15a1202d315
Adam Schiavone <adam8797@me.com>
2020-04-23 22:04:57 -0400

    Fixed build, added perm check to QR

--- src/WePrint/Controllers/QrCodeController.cs
+++ src/WePrint/Controllers/QrCodeController.cs
@@ -10,15 +10,21 @@ using WePrint.Controllers.Base;
 using WePrint.Models;
 using QRCoder;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using WePrint.Permissions;
+
 namespace WePrint.Controllers
 {
     [ApiController]
     [Route("api/qrcodes")]
     public class QrCodeController : WePrintController
     {
-        public QrCodeController(IServiceProvider services) : base(services)
+        private readonly IPermissionProvider<Project, ProjectCreateModel> _projectPermissionProvider;
+
+        public QrCodeController(IServiceProvider services, IPermissionProvider<Project, ProjectCreateModel> projectPermissionProvider) : base(services)
         {
+            _projectPermissionProvider = projectPermissionProvider;
         }
 
         [Authorize]
@@ -29,7 +35,6 @@ namespace WePrint.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetQrCode(Guid pledgeId)
         {
-            var s = ;
             var currentUser = await CurrentUser;
             if (currentUser == null)
                 return Unauthorized();
@@ -40,11 +45,12 @@ namespace WePrint.Controllers
             if (pledge.Maker != currentUser)
                 return Forbid();
 
-            
             QRCodeGenerator gen = new QRCodeGenerator();
-            Bitmap qrCodeImg = (new QRCode(gen.CreateQrCode($"{HttpContext.Request.Host}{Url.Action("ScanQrCode", new { pledgeId })}", QRCodeGenerator.ECCLevel.Q))).GetGraphic(20);
-            ImageConverter converter = new ImageConverter();
-            return File((byte[])converter.ConvertTo(qrCodeImg, typeof(byte[])), "image/jpeg");
+            Bitmap qrCodeImg = (new QRCode(gen.CreateQrCode($"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}{Url.Action("ScanQrCode", new { pledgeId })}", QRCodeGenerator.ECCLevel.Q))).GetGraphic(20);
+
+            await using var ms = new MemoryStream();
+            qrCodeImg.Save(ms, ImageFormat.Png);
+            return File(ms.ToArray(), "image/png", "qrcode.png");
         }
 
         [HttpGet("scan/{pledgeId}")]
@@ -55,6 +61,9 @@ namespace WePrint.Controllers
             var pledge = await Database.Pledges.FindAsync(pledgeId);
             if (pledge == null)
                 return NotFound();
+
+            if (!await _projectPermissionProvider.AllowWrite(await CurrentUser, pledge.Project))
+                return Forbid();
 
             pledge.Status = PledgeStatus.Finished;
             await Database.SaveChangesAsync();
