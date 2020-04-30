97f6eb35706fc6c0331a7c6c16b7294bd895e97e
Adam Schiavone <adam8797@me.com>
2020-04-30 17:47:59 -0400

    Added link to frontend to Admin pages

---
+++ src/WePrint/ReactRouter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WePrint.Data;
+using WePrint.Models;
+
+namespace WePrint
+{
+    public static class ReactRouter
+    {
+        public static string GetReactUrl(this IUrlHelper url, IIdentifiable<Guid> obj)
+        {
+            switch (obj)
+            {
+                case Organization org:
+                    return "/organization/" + org.Id;
+                
+                case Project proj:
+                    return "/project/" + proj.Id;
+                
+                default:
+                    return "#";
+            }
+        }
+    }
+}
